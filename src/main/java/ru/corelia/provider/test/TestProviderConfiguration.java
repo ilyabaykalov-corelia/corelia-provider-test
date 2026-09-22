3d8bfd69592a939f9f2f0b175a0f6be5dd678e2b
ilyabaykalov <ibaykalov@blogic.ru>
2026-09-22 14:56:09 +0300

    Проверены capabilities test provider при старте

--- src/main/java/ru/corelia/provider/test/TestProviderConfiguration.java
+++ src/main/java/ru/corelia/provider/test/TestProviderConfiguration.java
@@ -16,6 +16,10 @@ import ru.corelia.provider.DocumentVersionStore;
 import ru.corelia.provider.PermissionProvider;
 import ru.corelia.provider.TaskProvider;
 import ru.corelia.provider.WorkflowProvider;
+import ru.corelia.provider.ProviderCapability;
+import ru.corelia.provider.ProviderDescriptor;
+import ru.corelia.provider.ProviderStartupValidator;
+import ru.corelia.config.CoreliaRuntimeConfig;
 
 /** Минимальный provider для проверки независимого старта Corelia. */
 @AutoConfiguration
@@ -31,6 +35,10 @@ public class TestProviderConfiguration {
     @Bean BinaryStorage binaryStorage() { return capability(BinaryStorage.class); }
     @Bean AttachmentCatalog attachmentCatalog() { return capability(AttachmentCatalog.class); }
     @Bean PermissionProvider permissionProvider() { return capability(PermissionProvider.class); }
+    @Bean ProviderDescriptor providerDescriptor(@org.springframework.beans.factory.annotation.Qualifier("coreliaRuntimeConfig") CoreliaRuntimeConfig config) { ProviderDescriptor descriptor = new ProviderDescriptor() {
+        public String id() { return "test"; }
+        public java.util.Set<ProviderCapability> capabilities() { return java.util.EnumSet.allOf(ProviderCapability.class); }
+    }; ProviderStartupValidator.validate(config.provider(), descriptor); return descriptor; }
     @Bean AuthKeyProvider authKeyProvider() { return () -> ""; }
     @Bean AuthIdentityProvider authIdentityProvider() { return new AuthIdentityProvider() { public String issuer() { return "test"; } public java.util.Set<String> audiences() { return java.util.Set.of("corelia-web"); } }; }
     @Bean PermissionChecker permissionChecker(PermissionProvider provider) { return provider::require; }
